e20efa0a236f6ca2c1515b822762790249eed808
Niidaddey2025 <sdardey1221@gmail.com>
2025-07-11 12:18:37 +0000

    Done with Maxwell

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -1,5 +1,3 @@
-const BASE_URL = 'http://localhost:5000';
-
-// const BASE_URL = 'https://novabackend-production-b2ec.up.railway.app';
+const BASE_URL = 'https://novabackend-production-b2ec.up.railway.app';
 
 export default BASE_URL;
